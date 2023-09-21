7db35cfcf3eadd1dfb175658e1f8729d04601af9
chrille0234 <madsen.chr@hotmail.com>
2023-09-21 10:23:05 +0200

    add code to shopcategorylist.js

--- shopcategorylistJS/shopcategorylist.js
+++ shopcategorylistJS/shopcategorylist.js
@@ -1,51 +1,29 @@
 const CATEGORIES_UL = document.getElementById("categories__ul")
-
-const categories = [
-    {
-        name: "amplifier",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },
-    {
-        name: "Speakers",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "Turntables",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "CD Players",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "Streamers",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "Cables",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "Furniture",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "Headphones",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "Home Cinema",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "Ceiling Speakers",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "TV",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "Vinyl",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    },    {
-        name: "Outlet - Save ££££",
-        subcategories: ["integrated amplifiers", "power amplifiers", "preamplifiers"]
-    }
-]
-categories.forEach(category => {
-    const LI = document.createElement("li")
-    LI.innerText = category.name
-    LI.className = "categories__ulItem"
-    CATEGORIES_UL.appendChild(LI)
-})
+fetch("http://localhost:3000/categories")
+    .then(response => response.json())
+    .then(category => {
+        category.forEach(category =>{
+           const LI = document.createElement("li")
+            const SUB_UL = document.createElement("ul")
+            LI.innerHTML = category.category
+            LI.id = category.category
+            let hasBeenAppended = false
+            LI.addEventListener("click", function(){
+                if (category.subcategories !== undefined && !hasBeenAppended){
+                    category.subcategories.forEach(sub => {
+                        const SUB_LI = document.createElement("li")
+                        const SUB_A = document.createElement("a")
+                        SUB_A.href = `#${sub}`
+                        SUB_A.innerHTML = sub
+                        SUB_LI.appendChild(SUB_A)
+                        
+                        SUB_UL.appendChild(SUB_LI)
+                    })
+                    hasBeenAppended = true
+                }
+            })
+            
+            CATEGORIES_UL.appendChild(LI)
+            CATEGORIES_UL.appendChild(SUB_UL)
+       })
+    })
