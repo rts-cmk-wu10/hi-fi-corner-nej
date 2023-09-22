94a62fe36781674b2b88bce8ce106924870c760e
chrille0234 <madsen.chr@hotmail.com>
2023-09-22 12:53:08 +0200

    optimize code

--- shopcategorylistJS/shopcategorylist.js
+++ shopcategorylistJS/shopcategorylist.js
@@ -1,37 +1,40 @@
-const CATEGORIES_UL = document.getElementById("categories__ul")
-fetch("http://localhost:3000/categories")
-    .then(response => response.json())
-    .then(category => {
-        category.forEach((category, index) =>{
-           const LI = document.createElement("li")
-            const SUB_UL = document.createElement("ul")
-            LI.innerHTML = category.category
-            LI.className = "categories__ulItem"
-            
-            if (index === 0) LI.classList.add("categories__ulItem_active")
-            
-            LI.classList.add(`categories__${category.category}`)
-            LI.id = category.category
-            let hasBeenAppended = false
-            LI.addEventListener("click", function(){
-                if(category.subcategories !== undefined && !hasBeenAppended){
-                    category.subcategories.forEach(sub => {
-                        console.log(123)
-                        const SUB_LI = document.createElement("li")
-                        const SUB_A = document.createElement("a")
-                        SUB_A.href = `#${sub}`
-                        SUB_A.innerHTML = sub
-                        SUB_LI.appendChild(SUB_A)
+const CATEGORIES_UL = document.getElementById("categories__ul");
 
-                        SUB_UL.appendChild(SUB_LI)
-                    })
+async function fetchCategories() {
+    try {
+        const response = await fetch("http://localhost:3000/categories");
+        const categories = await response.json();
+
+        categories.forEach((category, index) => {
+            const LI = document.createElement("li");
+            const SUB_UL = document.createElement("ul");
+            LI.innerHTML = category.category;
+            LI.className = "categories__ulItem";
+
+            if (index === 0) LI.classList.add("categories__ulItem_active");
+
+            LI.classList.add(`categories__${category.category}`);
+            LI.id = category.category;
+
+            LI.addEventListener("click", function () {
+                if (category.subcategories && category.subcategories.length > 0 && !SUB_UL.hasChildNodes()) {
+                    category.subcategories.forEach((sub) => {
+                        const SUB_LI = document.createElement("li");
+                        const SUB_A = document.createElement("a");
+                        SUB_A.href = `#${sub}`;
+                        SUB_A.textContent = sub;
+                        SUB_LI.appendChild(SUB_A);
+                        SUB_UL.appendChild(SUB_LI);
+                    });
                 }
+            });
 
-                    hasBeenAppended = true
-                
-            })
-            
-            CATEGORIES_UL.appendChild(LI)
-            CATEGORIES_UL.appendChild(SUB_UL)
-       })
-    })
+            CATEGORIES_UL.appendChild(LI);
+            CATEGORIES_UL.appendChild(SUB_UL);
+        });
+    } catch (error) {
+        console.error("Error fetching categories:", error);
+    }
+}
+
+fetchCategories();
