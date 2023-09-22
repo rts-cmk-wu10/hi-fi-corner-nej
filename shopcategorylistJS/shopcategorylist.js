e6bc32ac2519274273292aa500aa1751ebcbe0c4
chrille0234 <madsen.chr@hotmail.com>
2023-09-22 10:18:53 +0200

    remove code from style.css, add shopcategorylist.css

--- shopcategorylistJS/shopcategorylist.js
+++ shopcategorylistJS/shopcategorylist.js
@@ -2,26 +2,33 @@ const CATEGORIES_UL = document.getElementById("categories__ul")
 fetch("http://localhost:3000/categories")
     .then(response => response.json())
     .then(category => {
-        category.forEach(category =>{
+        category.forEach((category, index) =>{
            const LI = document.createElement("li")
             const SUB_UL = document.createElement("ul")
             LI.innerHTML = category.category
             LI.className = "categories__ulItem"
+            
+            if (index === 0) LI.classList.add("categories__ulItem_active")
+            
+            LI.classList.add(`categories__${category.category}`)
             LI.id = category.category
             let hasBeenAppended = false
             LI.addEventListener("click", function(){
-                if (category.subcategories !== undefined && !hasBeenAppended){
+                if(category.subcategories !== undefined && !hasBeenAppended){
                     category.subcategories.forEach(sub => {
+                        console.log(123)
                         const SUB_LI = document.createElement("li")
                         const SUB_A = document.createElement("a")
                         SUB_A.href = `#${sub}`
                         SUB_A.innerHTML = sub
                         SUB_LI.appendChild(SUB_A)
-                        
+
                         SUB_UL.appendChild(SUB_LI)
                     })
-                    hasBeenAppended = true
                 }
+
+                    hasBeenAppended = true
+                
             })
             
             CATEGORIES_UL.appendChild(LI)
