adcdf4b8505295257bbc1a2c1f29b30194f08c0b
Chrille0234 <madsen.chr@hotmail.com>
2023-09-26 12:03:49 +0200

    add proper manufacturer counter

--- shopcategorylistJS/priceAndManufacturer.js
+++ shopcategorylistJS/priceAndManufacturer.js
@@ -6,23 +6,22 @@ const UNDER4000 = document.getElementById("under4000");
 
 async function getPrices() {
     const RESPONSE = await fetch("http://localhost:3000/products");
-    const product = await RESPONSE.json();
+    const products = await RESPONSE.json();
 
     let under1000_count = 0;
     let under2000_count = 0;
     let under3000_count = 0;
     let under4000_count = 0;
 
-    product.forEach((price) => {
-        console.log(price.title)
-        price = price.price;
+    products.forEach((product) => {
+        console.log(product.title)
+        price = product.price;
 
         if (price <= 1000) under1000_count++;
         if (price > 1000 && price <= 2000) under2000_count++;
         if (price > 2000 && price <= 3000) under3000_count++;
         if (price > 3000 && price <= 4000) under4000_count++;
     });
-
     UNDER1000.innerHTML = under1000_count;
     UNDER2000.innerHTML = under2000_count;
     UNDER3000.innerHTML = under3000_count;
@@ -50,4 +49,18 @@ async function getManufacturers() {
     });
 }
 
+// counts how many times a manufacture shows up in the products array
+const manufacturerCounts = {};
+async function countManufacturers() {
+    const RESPONSE = await fetch("http://localhost:3000/products");
+    const products = await RESPONSE.json();
+
+      products.forEach((product) => {
+        manufacturer = product.manufacturer;
+        if (manufacturerCounts[manufacturer]) manufacturerCounts[manufacturer]++;
+        else manufacturerCounts[manufacturer] = 1;
+    })
+}
+countManufacturers();
 getManufacturers();
+countManufacturers()
